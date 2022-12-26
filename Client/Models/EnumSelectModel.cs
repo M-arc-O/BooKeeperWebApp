e3d279b37aa59f4f336b34618b08b9b5b979be8d
Marco Trapman <marco.trapman@teamrockstars.nl>
2022-12-26 19:22:03 +0100

    Fixed showing of account type

--- Client/Models/EnumSelectModel.cs
+++ Client/Models/EnumSelectModel.cs
@@ -2,6 +2,18 @@
 
 public class EnumSelectModel<T>
 {
-    public T Enum { get; set; }
+    public T EnumValue { get; set; }
     public string EnumString { get; set; }
+
+    public static List<EnumSelectModel<T>> GetEnumModel()
+    {
+        var retVal = new List<EnumSelectModel<T>>();
+        var types = Enum.GetValues(typeof(T)).Cast<T>();
+        foreach (var type in types)
+        {
+            retVal.Add(new EnumSelectModel<T> { EnumValue = type, EnumString = type!.ToString()! });
+        }
+
+        return retVal;
+    }
 }
